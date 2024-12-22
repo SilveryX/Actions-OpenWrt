d8c36247b46e6d70fc0daafaf0d06a5ac53c0001
SilveryX <67977075+SilveryX@users.noreply.github.com>
2024-12-22 08:51:46 +0800

    Update diy-part1.sh

--- X86_64/diy-part1.sh
+++ X86_64/diy-part1.sh
@@ -20,3 +20,4 @@ echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.defa
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
